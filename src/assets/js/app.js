4d419cc94d5333163d71ec0c05d7cf28c1e7faea
Anton Mayatskiy <work.mayatskiy@gmail.com>
2020-03-26 09:37:44 +1000

    Modal created

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,21 @@
 import 'what-input';
 
+
 //polyfill
 import './utils/polyfill';
 
 //modules
 // import './modules/CustomScroll';
+
+
+$('[data-open-modal]').on('click', function() {
+  $('html').addClass('page--modal-opened');
+});
+
+// modal opening
+$(document).on('click', function(e) {
+
+  if($(e.target).attr('data-close-modal')) {
+    $('html').removeClass('page--modal-opened');
+  }
+});
