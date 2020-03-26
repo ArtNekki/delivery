b5818e553a0f0f85d828817a08f9f6b6c6bb6132
Anton Mayatskiy <work.mayatskiy@gmail.com>
2020-03-26 11:38:17 +1000

    Counter finished

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,6 +4,21 @@ import 'what-input';
 //polyfill
 import './utils/polyfill';
 
+$(document).ready(function () {
+
+  $('.promo-item b').each(function () {
+    $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+    }, {
+      duration: 4000,
+      easing: 'swing',
+      step: function (now) {
+        $(this).text(Math.ceil(now));
+      }
+    });
+  });
+});
+
 $('[data-open-modal]').on('click', function() {
   $('html').addClass('page--modal-opened');
 });
